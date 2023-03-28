8d649df766b127875add17262b0b4890b7c7b41a
matias225 <mati.kbza22@gmail.com>
2023-03-28 17:56:45 -0300

    CRUD Sucursal

--- shipping_microservice/main/repositories/__init__.py
+++ shipping_microservice/main/repositories/__init__.py
@@ -2,3 +2,4 @@ from .base_repository import Create, Update, Delete, Read
 from .order_repository import OrderRepository
 from .ship_repository import ShipRepository
 from .articlesorder_repository import ArticlesOrderRepository
+from .sucursal_repository import SucursalRepository
