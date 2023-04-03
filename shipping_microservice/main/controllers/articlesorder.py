9183f5656f506c4af2d3edca673f4e0f158d9f72
GastonFenske <thesacha2000f@gmail.com>
2023-04-03 10:53:19 -0300

    test order_service and ship_service almost complete

--- shipping_microservice/main/controllers/articlesorder.py
+++ shipping_microservice/main/controllers/articlesorder.py
@@ -9,16 +9,16 @@ stock_service = StockService()
 class ArticlesOrderController(Resource):
 
     def post(self):
-        articlesorder = articlesorder_schema.load(request.get_json())
-        return articlesorder_schema.dump(stock_service.add_articlesorder(articlesorder))
+        articles_order = articlesorder_schema.load(request.get_json())
+        # return articlesorder_schema.dump(stock_service.add_articlesorder(articlesorder))
     
-    def get(self):
-        return articlesorder_schema.dump(stock_service.get_articlesorders(), many=True)
+    def get(self): ...
+        # return articlesorder_schema.dump(stock_service.get_articlesorders(), many=True)
 
     def put(self):
-        articlesorder = articlesorder_schema.load(request.get_json())
-        return articlesorder_schema.dump(stock_service.edit_articlesorder(articlesorder))
+        articles_order = articlesorder_schema.load(request.get_json())
+        # return articlesorder_schema.dump(stock_service.edit_articlesorder(articlesorder))
     
     def delete(self):
-        articlesorder = articlesorder_schema.load(request.get_json())
-        return articlesorder_schema.dump(stock_service.delete_articlesorder(articlesorder))
+        articles_order = articlesorder_schema.load(request.get_json())
+        # return articlesorder_schema.dump(stock_service.delete_articlesorder(articlesorder))
