78f6f1b9553262a162e1011ab26a32af9d8b9048
GastonFenske <thesacha2000f@gmail.com>
2023-04-04 18:42:39 -0300

    docker compose in main

--- articles_microservice/main/__init__.py
+++ articles_microservice/main/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
-import mysql.connector
+# import mysql.connector
 from flask_restful import Api
 
 api = Api()
@@ -12,16 +12,22 @@ def create_app():
     app = Flask(__name__)
     load_dotenv()
 
-
     HOST = os.getenv("DB_HOST")
     USER = os.getenv("DB_USER")
     PASSWORD = os.getenv("DB_PASSWORD")
-    PORT = int(os.getenv("DB_PORT"))
+    PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_DATABASE")
+
+    print(f'HOST: {HOST}')
+    print(f'USER: {USER}')
+    print(f'PASSWORD: {PASSWORD}')
+    print(f'PORT: {PORT}')
+    print(f'DB_NAME: {DB_NAME}')
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}'
 
     db.init_app(app)
 
